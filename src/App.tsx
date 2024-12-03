b9d1236513a08e72059e0a04f7731a44303f966b
AlysonMatheus <alyson2000@live.com>
2024-12-03 18:11:45 -0300

    att

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios"; 
 
-function App() {
+
+interface Tarefa {
+  id: number;
+  titulo: string;
+  descricao: string;
+  concluida: boolean;
+}
+
+const App: React.FC = () => {
+  const [tarefas, setTarefas] = useState<Tarefa[]>([]); 
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null); 
+  useEffect(() => {
+  
+    const fetchTarefas = async () => {
+      try {
+        const response = await axios.get("http://localhost:3000/api/tarefa"); 
+        setTarefas(response.data);
+        setLoading(false); 
+      } catch (error) {
+        console.error("Erro ao buscar tarefas:", error); 
+        setError("Erro ao carregar tarefas"); // 
+        setLoading(false);
+      }
+    };
+
+    fetchTarefas(); 
+  }, []);
+
+  if (loading) {
+    return <div>Carregando tarefas...</div>; // 
+  }
+
+  if (error) {
+    return <div>{error}</div>; 
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Lista de Tarefas</h1>
+        <ul>
+          {tarefas.map((tarefa) => (
+            <li key={tarefa.id}>
+              <h3>{tarefa.titulo}</h3>
+              <p>{tarefa.descricao}</p>
+              <span>{tarefa.concluida ? "Concluída" : "Não concluída"}</span>
+            </li>
+          ))}
+        </ul>
       </header>
     </div>
   );
-}
+};
 
 export default App;
--- src/backend/app.ts/app.ts
+++ src/backend/app.ts/app.ts
@@ -1,18 +1,18 @@
 import express from "express";
 import cors from "cors";
-import rotasTarefas from "./rotas/rotasTarefas";
-import { AppDataSource } from "./backend/configuracao/bancoDados";
-
-AppDataSource.initialize()
-  .then(() => {
-    console.log("Banco de dados conectado");
-  })
-  .catch((err: any) => console.error("Falha na conexão com o banco", err));
+import rotasTarefas from "../rotas/rotasTarefas";
+import { AppDataSource } from "../configuracao/bancoDados";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/tarefas", rotasTarefas);
+app.use("/api", rotasTarefas);
 
-export default app;
+AppDataSource.initialize()
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Servidor rodando em http://localhost:3000");
+    });
+  })
+  .catch((error) => console.log("Erro ao conectar ao banco de dados:", error));
